656c9326ea9c36efa0ecc21a325fe25bb14ebd0c
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-07-19 20:31:25 +0300

    added output property binding

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,5 +1,6 @@
 export class Goal {
-id:number=0;
-name:string="";
-description:string=" ";
+    showDescription: boolean;
+    constructor(public id: number=0,public name: string="",public description: string=""){
+      this.showDescription=false;
+    }
 }
