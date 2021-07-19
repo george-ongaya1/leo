0067e571425571be3b2f907567ea7d7451b82975
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-07-19 20:34:20 +0300

    added the goal properties in the constructor

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,6 +1,10 @@
 export class Goal {
     showDescription: boolean;
-    constructor(public id: number=0,public name: string="",public description: string=""){
+    constructor(
+        public id: number=0,
+        public name: string="",
+        public description: string="")
+        {
       this.showDescription=false;
     }
 }
