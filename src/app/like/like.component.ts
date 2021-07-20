4e4049dfb9b18e301ea31bc173270ae8105b4422
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-07-20 12:20:49 +0300

    styled the like and dislike buttons

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -3,16 +3,16 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'like-box',
   templateUrl: './like.component.html',
- 
+
 })
-export class LikeComponent{
-numberOflikes:number=0;
+export class LikeComponent {
+  numberOflikes: number = 0;
 
-likeButtonclick(){
+  likeButtonclick() {
+    this.numberOflikes++;
+  }
 
-}
- 
-}
-likeButtonclick(){
-
+  dislikeButtonclick() {
+    this.numberOflikes--;
+  }
 }
